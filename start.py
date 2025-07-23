90f168d3b20605823ea8f0fd4c543659bf9342ff
VadymCov <vadymkravtcsov@gmail.com>
2025-07-23 16:44:10 +0300

    add callbak handlers for 3 buttons

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+import sqlite3
+from aiogram import  Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import CallbackQuery, Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import YOUR_TOKEN # Important: config.py is added to .gitignore to prevent token leakage.
                             
 
@@ -9,18 +10,34 @@ bot = Bot(token=YOUR_TOKEN) # Initialize the bot using the token from config.py.
 db = Dispatcher()
 
 
+    
 @db.message(CommandStart())
 async def cmd_start(message:Message):
         markup = InlineKeyboardMarkup(
             inline_keyboard=[
                 [
-                    InlineKeyboardButton(text='???', callback_data="?"),
-                    InlineKeyboardButton(text='???', callback_data="?"),
-                    InlineKeyboardButton(text='???', callback_data="?")
+                    InlineKeyboardButton(text='one', callback_data="btn_one"),
+                    InlineKeyboardButton(text='two', callback_data="btn_two"),
+                    InlineKeyboardButton(text='three', callback_data="btn_three")
                 ]
             ]
         )
         await message.answer(f"Hello", reply_markup=markup)
+
+@db.callback_query(F.data=="btn_one")
+async def handle_btn_one(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer("Button one")
+
+@db.callback_query(F.data=="btn_two")
+async def handle_btn_two(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer("Button two")
+
+@db.callback_query(F.data=="btn_three")
+async def handle_btn_three(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer("Button three")
 
 
 async def main():
@@ -28,4 +45,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-    print("bot start")
